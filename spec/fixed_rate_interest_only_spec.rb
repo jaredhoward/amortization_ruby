f079381b9c5a03a141feebe49d6ea35cc3177d8d
Jared Howard <jared@howardpants.com>
2013-03-05 22:42:32 -0700

    add another fixed rate interest only test

--- spec/fixed_rate_interest_only_spec.rb
+++ spec/fixed_rate_interest_only_spec.rb
@@ -2,7 +2,8 @@ require_relative '../lib/loan'
 
 describe Loan do
   [
-    {amount: 345_000, interest_rate: 7.75, interest_only_period: 120, total_prepaid_finance_amount: 2859.75, first_pi: 2228.13, normal_pi: 2832.27, last_pi: 2833.69, apr: 7.830}
+    {amount: 345_000, interest_rate: 7.75, interest_only_period: 120, total_prepaid_finance_amount: 2859.75, first_pi: 2228.13, normal_pi: 2832.27, last_pi: 2833.69, apr: 7.830},
+    {amount: 355_000, interest_rate: 6.5, interest_only_period: 120, total_prepaid_finance_amount: 4890.72, first_pi: 1922.92, normal_pi: 2646.78, last_pi: 2648.94, apr: 6.621}
   ].each do |loan_hash|
     context "fixed rate interest only" do
       let(:loan) { Loan.new(loan_hash) }
