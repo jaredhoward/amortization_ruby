0781e25be685d0af875d43f37ddb92a151a3723e
Jared Howard <jared@howardpants.com>
2013-03-05 20:39:05 -0700

    extend Class with #extended_attr_writer and #extended_attr_accessor methods

---
+++ lib/extend/custom_class.rb
@@ -0,0 +1,45 @@
+class Class
+
+  def extended_attr_accessor(*args)
+    original_args = args.dup
+    options = args.last.is_a?(Hash) ? args.pop : {}
+
+    args.each do |attribute|
+      self.class_eval("def #{attribute};@#{attribute};end")
+    end
+
+    self.extended_attr_writer(original_args)
+  end
+
+  def extended_attr_writer(*args)
+    args.flatten!
+    options = args.last.is_a?(Hash) ? args.pop : {}
+
+    args.each do |attribute|
+      @extended_attributes = {} unless @extended_attributes
+      @extended_attributes[attribute.to_sym] = options
+
+      self.class_eval <<-EOC
+        def #{attribute}=(value)
+          ext = self.class.instance_variable_get(:@extended_attributes)[:#{attribute}]
+
+          unless value.nil?
+            if ext[:kind_of]
+              raise ArgumentError, "\#\{self.class.to_s\}##{attribute}= \#\{value.inspect\} is not the correct data type" unless value.kind_of?(ext[:kind_of])
+            end
+            if ext[:validate]
+              if ext[:validate].instance_of?(Proc)
+                raise ArgumentError, "\#\{self.class.to_s\}##{attribute}= \#\{value.inspect\} is not valid" unless ext[:validate].call(value)
+              end
+            end
+          end
+
+          [ext[:reset]].flatten.each {|reset| instance_variable_set(:"@\#\{reset\}", nil) } if ext[:reset]
+
+          @#{attribute} = value
+        end
+      EOC
+    end
+  end
+
+end
